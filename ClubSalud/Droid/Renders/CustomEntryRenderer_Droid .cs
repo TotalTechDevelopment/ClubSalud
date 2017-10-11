68ad40c3d60b41ddf894456f9966be2b39d535a6
DESKTOP-0J3HE0J\Edgar Pérez <edgar.mope.os@gmail.com>
2017-10-11 14:44:03 -0500

    *EditText Android bottom line removed

---
+++ ClubSalud/Droid/Renders/CustomEntryRenderer_Droid .cs
@@ -0,0 +1,17 @@
+﻿using ClubSalud.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer_Droid))]
+namespace ClubSalud.Droid
+{
+    public class CustomEntryRenderer_Droid : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
